d4e36a0f3d9cb7eac67b14afea020df75b0fa88e
ignisilva92 <ignisilva92@gmail.com>
2022-03-21 23:43:07 +0900

    Update 2525.js

--- 2525.js
+++ 2525.js
@@ -1,5 +1,5 @@
 let fs = require('fs');
-let input = fs.readFileSync('input.txt').toString().trim().split('\n');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 let result = "";
 let [hour, min] = input[0].split(' ').map(Number);
