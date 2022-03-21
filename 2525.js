1db371520aba0f50c646cd815298ee493957bd77
ignisilva92 <ignisilva92@gmail.com>
2022-03-21 23:42:41 +0900

    feat: solve 2525

---
+++ 2525.js
@@ -0,0 +1,19 @@
+let fs = require('fs');
+let input = fs.readFileSync('input.txt').toString().trim().split('\n');
+
+let result = "";
+let [hour, min] = input[0].split(' ').map(Number);
+const cookTime = Number(input[1]);
+
+min += cookTime;
+
+if(min >= 60) {
+  hour += Math.floor(min/60);
+  min = min%60;
+}
+
+if(hour >= 24) {
+  hour = hour%24;
+}
+
+console.log(hour, min);
